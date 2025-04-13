a44a8e5f7a266372ee9749e35114e310e1ae4bf5
348ab1a8-424a-43d8-b463-8e925d751c7f <348ab1a8-424a-43d8-b463-8e925d751c7f@amplivum.com>
2025-04-12 20:14:01 -0400

    Pivoting to use Reddit API instead.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using SeleniumExtras.WaitHelpers;
-using System;
-
-namespace MaintainRedditStreak
+﻿namespace MaintainRedditStreak
 {
     internal class MaintainRedditStreak
     {
@@ -15,57 +9,7 @@ namespace MaintainRedditStreak
 
         private static void UpvoteFirstPost()
         {
-            string userDir = string.Empty;
 
-            try
-            {
-                string _filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.txt");
-                userDir = File.ReadLines(_filePath).First();
-            }
-            catch (DirectoryNotFoundException)
-            {
-                Console.WriteLine("Config.txt file not found in executable directory.");
-            }
-
-            var options = new ChromeOptions();
-            options.AddArgument("--start-maximized");
-            options.AddArgument($"{userDir}");
-
-            IWebDriver driver = new ChromeDriver(options);
-
-            try
-            {
-                driver.Navigate().GoToUrl("https://www.reddit.com/");
-
-                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
-                wait.Until(ExpectedConditions.ElementExists(By.CssSelector("button[upvote]")));
-
-                var upvoteButtons = driver.FindElements(By.CssSelector("button[upvote]"));
-
-                if (upvoteButtons.Count > 0)
-                {
-                    upvoteButtons[0].Click();
-                    Console.WriteLine("Succesfully upvoted the first post.");
-                }
-                else
-                {
-                    Console.WriteLine("Could not find any upvote buttons.");
-                }
-
-                Thread.Sleep(2000);
-            }
-            catch (WebDriverTimeoutException)
-            {
-                Console.WriteLine("Web driver timed out waiting for elements to load.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occured: " + ex.Message);
-            }
-            finally
-            {
-                driver.Quit();
-            }
         }
     }
 }
